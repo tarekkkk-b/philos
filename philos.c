1f5b769ad03163e8ad31034e32142e03e53baca2
Tarek Badawi <tabadawi@student.42abudhabi.ae>
2024-05-17 18:42:47 +0400

    temp

--- philos.c
+++ philos.c
@@ -6,7 +6,7 @@
 /*   By: tabadawi <tabadawi@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:25:33 by tabadawi          #+#    #+#             */
-/*   Updated: 2024/05/14 21:04:18 by tabadawi         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:28:03 by tabadawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,83 +17,97 @@
 
 typedef	struct s_philos
 {
-	int	philos;
-	int	*ID;
-	pthread_mutex_t eating;
-	pthread_mutex_t sleeping;
-	pthread_mutex_t thinking;
+	int	ID;
 	pthread_t	*philo;
 }	t_philos;
 
-void	check_inp(int ac, char **av, t_philos *philo)
+typedef	struct	t_shared
+{
+	int	philos;
+	t_philos	**philo;
+	pthread_mutex_t eating;
+	pthread_mutex_t sleeping;
+	pthread_mutex_t thinking;
+}	t_shared;
+
+void	check_inp(int ac, char **av, t_shared *shared)
 {
 	if (ac < 5 || ac > 6)
 		exit(1);
-	philo->philos = atoi(av[1]);
+	shared->philos = atoi(av[1]);
 	int i = -1;
-	philo->ID = malloc(sizeof(int) * philo->philos);
-	while (++i < philo->philos)
-		philo->ID[i] = i + 1;
-	philo->philo = malloc(sizeof(pthread_t) * philo->philos);
-}
-
-void	eating(t_philos *philo)
-{
-	pthread_mutex_lock(&philo->eating);
-	printf("i ate\n");
-	pthread_mutex_unlock(&philo->eating);
-}
-
-void	sleeping(t_philos *philo)
-{
-	pthread_mutex_lock(&philo->sleeping);
-	printf("i slept\n");
-	pthread_mutex_unlock(&philo->sleeping);
-}
-
-void	thinking(t_philos *philo)
-{
-	pthread_mutex_lock(&philo->thinking);
-	printf("i thought\n");
-	pthread_mutex_unlock(&philo->thinking);
-}
-
-void	*routine(void *philo)
-{
-	while (1)
-	{	
-		eating(philo);
-		sleeping(philo);
-		thinking(philo);
+	shared->philo = malloc(sizeof(t_philos) * shared->philos + 1);
+	while (++i < shared->philos)
+	{
+		printf("hi");
+		shared->philo[i]->ID = i + 1;
+		shared->philo[i]->philo = malloc(sizeof(pthread_t));
 	}
-	return (NULL);
+	// while (++i < philo->philos)
+	// 	philo->ID[i] = i + 1;
+	// philo->philo = malloc(sizeof(pthread_t) * philo->philos);
 }
 
-void	create_threads(t_philos *philo)
-{
-	int i = -1;
-	while (++i < philo->philos)
-		pthread_create(&philo->philo[i], NULL, routine, (void *)philo);
-}
+// void	eating(t_philos *philo)
+// {
+// 	pthread_mutex_lock(&philo->eating);
+// 	printf("i ate\n");
+// 	pthread_mutex_unlock(&philo->eating);
+// }
 
-void	joiner(t_philos *philo)
-{
-	int i = -1;
-	while (++i < philo->philos)
-		pthread_join(philo->philo[i], NULL);
-}
+// void	sleeping(t_philos *philo)
+// {
+// 	pthread_mutex_lock(&philo->sleeping);
+// 	printf("i slept\n");
+// 	pthread_mutex_unlock(&philo->sleeping);
+// }
+
+// void	thinking(t_philos *philo)
+// {
+// 	pthread_mutex_lock(&philo->thinking);
+// 	printf("i thought\n");
+// 	pthread_mutex_unlock(&philo->thinking);
+// }
+
+// void	*routine(void *philo)
+// {
+// 	while (1)
+// 	{	
+// 		eating(philo);
+// 		sleeping(philo);
+// 		thinking(philo);
+// 	}
+// 	return (NULL);
+// }
+
+// void	create_threads(t_philos *philo)
+// {
+// 	int i = -1;
+// 	while (++i < philo->philos)
+// 		pthread_create(&philo->philo[i], NULL, routine, (void *)philo);
+// }
+
+// void	joiner(t_philos *philo)
+// {
+// 	int i = -1;
+// 	while (++i < philo->philos)
+// 		pthread_join(philo->philo[i], NULL);
+// }
 
 int	main(int ac, char **av)
 {
-	t_philos philo;
+	t_shared	shared;
 
-	pthread_mutex_init(&philo.eating, NULL);
-	pthread_mutex_init(&philo.sleeping, NULL);
-	pthread_mutex_init(&philo.thinking, NULL);
-	check_inp(ac, av, &philo);
-	create_threads(&philo);
-	pthread_mutex_destroy(&philo.eating);
-	pthread_mutex_destroy(&philo.sleeping);
-	pthread_mutex_destroy(&philo.thinking);
-	joiner(&philo);
+	// pthread_mutex_init(&philo.eating, NULL);
+	// pthread_mutex_init(&philo.sleeping, NULL);
+	// pthread_mutex_init(&philo.thinking, NULL);
+	check_inp(ac, av, &shared);
+	int i = 0;
+	while (i++ <= shared.philos)
+		printf("%d\n", shared.philo[i]->ID);
+	// create_threads(&philo);
+	// pthread_mutex_destroy(&philo.eating);
+	// pthread_mutex_destroy(&philo.sleeping);
+	// pthread_mutex_destroy(&philo.thinking);
+	// joiner(&philo);
 }
